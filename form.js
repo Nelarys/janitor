75c3b3a3c6098c73bfa41be82657ba0d6dd22e9a
Nelarys <77840649+Nelarys@users.noreply.github.com>
2025-10-16 20:23:42 +0200

    Add basic mobile version and beginnings of form
    so far the emails are gathered in a google sheet

---
+++ form.js
@@ -0,0 +1,35 @@
+const scriptUrl = 'https://script.google.com/macros/s/AKfycbwL6E_0pl6rbCqY6lmIqxA3MK8dxMLMrP0C7x-odvCqbd51tlGx797cO8dgpavmpqLDCQ/exec';
+const form = document.getElementById('mailingForm');
+const resultMessage = document.getElementById('responseMessage');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const data = {
+    email: form.email.value
+  };
+
+  fetch(scriptUrl, {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.text())
+  .then(result => {
+    console.log('HERE');
+    console.log(result);
+    if (result === 'duplicate') {
+      resultMessage.textContent = "you're already on the list"
+    } else if (result === 'success') {
+      resultMessage.textContent = "Added successfully"
+    } else {
+      resultMessage.textContent = "unexpected response :/"
+    }
+    form.reset();
+  })
+  .catch(err => {
+    console.error(err);
+    resultMessage.textContent = "failed to add to mailinglist"
+  })
+})
